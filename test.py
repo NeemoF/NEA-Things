2b39108be4dc635d53be8e92b8ef60a0b52df4fe
NeemoF <neemaf@outlook.com>
2022-12-06 13:48:00 +0000

    balls in ur draws

---
+++ test.py
@@ -0,0 +1,30 @@
+class player:
+    def __init__(self, name, type):
+        self.name = name
+        self.type= type
+    
+    def getName(self):
+        print(self.name)
+
+    def getType(self):
+        print(self.type)
+    
+    def setName(self, newName):
+        self.name = newName
+
+object = player("Neema", "Mage")
+
+thisDict = {
+    "Name" : object.getName,
+    "Two" : object.getType,
+    "Three" : object.setName,
+}
+
+thisList = [
+    object.getName,
+    object.getType,
+    object.setName
+]
+
+thisList[2]("James")
+thisList[0]()
